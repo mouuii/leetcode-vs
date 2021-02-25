1444cec74bdb8f7d86cc711b5e1f4080f6da7efd
mouui <chaoyueshijian@gmail.com>
2021-02-25 15:10:31 +0800

    03 滑动窗口

---
+++ 3.无重复字符的最长子串.go
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=golang
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+func lengthOfLongestSubstring(s string) int {
+	// 解法二 滑动窗口
+	if len(s) == 0 {
+		return 0
+	}
+	var freq [256]int
+	result, left, right := 0, 0, -1
+
+	for left < len(s) {
+		if right+1 < len(s) && freq[s[right+1]-'a'] == 0 {
+			freq[s[right+1]-'a']++
+			right++
+		} else {
+			freq[s[left]-'a']--
+			left++
+		}
+		result = max(result, right-left+1)
+	}
+	return result
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
+
