a929b96f65c42fdad1077680c63b39a408b22c16
mouui <chaoyueshijian@gmail.com>
2021-02-26 21:17:12 +0800

    homebrew

---
+++ 98.验证二叉搜索树.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=golang
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	return help(root, math.MinInt64, math.MaxInt64)
+}
+
+func help(root *TreeNode, lower, upper int) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val <= lower || root.Val >= upper {
+		return false
+	}
+	return help(root.Left, lower, root.Val) && help(root.Right, root.Val, upper)
+}
+
+// @lc code=end
+
