d554ceebc5c1f683ea0c1433d9254b0a9fd9a493
mouui <chaoyueshijian@gmail.com>
2021-02-26 12:38:16 +0800

    noon rest

---
+++ 242.有效的字母异位词.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=golang
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+func isAnagram(s string, t string) bool {
+	s1, s2 := []byte(s), []byte(t)
+	sort.Slice(s1, func(i, j int) bool {
+		return s1[i] < s1[j]
+	})
+	sort.Slice(s2, func(i, j int) bool {
+		return s2[i] < s2[j]
+	})
+	return string(s2) == string(s1)
+}
+
+// @lc code=end
+
---
+++ 49.字母异位词分组.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=golang
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+func groupAnagrams(strs []string) [][]string {
+	mp := map[string][]string{}
+	for _, str := range strs {
+		s := []byte(str)
+		sort.Slice(s, func(i, j int) bool {
+			return s[i] < s[j]
+		})
+		sortedStr := string(s)
+		mp[sortedStr] = append(mp[sortedStr], str)
+	}
+	ans := make([][]string, 0, len(mp))
+	for _, v := range mp {
+		ans = append(ans, v)
+	}
+	return ans
+}
+
+// @lc code=end
+
