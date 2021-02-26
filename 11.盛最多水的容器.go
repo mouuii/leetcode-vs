54646a41f607ce092accc9bfc923e1e9da1961a7
mouui <chaoyueshijian@gmail.com>
2021-02-26 19:25:05 +0800

    water

--- 11.盛最多水的容器.go
+++ 11.盛最多水的容器.go
@@ -6,31 +6,24 @@
 
 // @lc code=start
 func maxArea(height []int) int {
-	// 1.暴力法
-	// for i := 0; i < a.length-1; i++ {
-	// 	for j := i + 1; j < a.length; j++ {
-
-	// 	}
-	// }
-	// 2.双指针？
 	max, start, end := 0, 0, len(height)-1
 	for start < end {
 		width := end - start
 		high := 0
-
 		if height[start] < height[end] {
-			height = height[start]
+			high = height[start]
 			start++
 		} else {
-			height = height[left]
+			high = height[end]
 			end--
 		}
-		tmp := width * high
+
+		temp := width * high
 		if temp > max {
 			max = temp
 		}
-		return max
 	}
+	return max
 }
 
 // @lc code=end
