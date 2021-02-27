da19c8f66a0d9066f211003fdc71a113e60f3ecc
mouui <chaoyueshijian@gmail.com>
2021-02-27 15:29:41 +0800

    heap

---
+++ 215.数组中的第k个最大元素.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=golang
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+func findKthLargest(nums []int, k int) int {
+	heapSize := len(nums)
+	buildMaxHeap(nums, heapSize)
+	for i := len(nums) - 1; i >= len(nums)-k+1; i-- {
+		nums[0], nums[i] = nums[i], nums[0]
+		heapSize--
+		maxHeapify(nums, 0, heapSize)
+	}
+	return nums[0]
+}
+
+func buildMaxHeap(a []int, heapSize int) {
+	for i := heapSize / 2; i >= 0; i-- {
+		maxHeapify(a, i, heapSize)
+	}
+}
+
+func maxHeapify(a []int, i, heapSize int) {
+	l, r, largest := i*2+1, i*2+2, i
+	if l < heapSize && a[l] > a[largest] {
+		largest = l
+	}
+	if r < heapSize && a[r] > a[largest] {
+		largest = r
+	}
+	if largest != i {
+		a[i], a[largest] = a[largest], a[i]
+		maxHeapify(a, largest, heapSize)
+	}
+}
+
+// @lc code=end
+
